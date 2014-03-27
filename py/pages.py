013ae5d731131290738bb4ab5ebd8db6aa810dac
JoseOneTime <claveren@gmail.com>
2014-03-27 17:43:11 -0400

    Fix decorator and find_all bugs and whitespace

--- py/pages.py
+++ py/pages.py
@@ -6,13 +6,16 @@ from urlparse import urljoin
 from bs4 import BeautifulSoup
 import requests
 
-def catch_key_error(func, *args, **kwargs):
+def catch_key_error(func):
     """ Decorator logs key errors """
-    try:
-        return func(*args, **kwargs)
-    except KeyError as err:
-        print 'KeyError in %s: %s' % (func.__name__, err)
-        return None
+    def decorated(*args, **kwargs):
+        result = None
+        try:
+            result = func(*args, **kwargs)
+        except KeyError as err:
+            print 'KeyError in %s: %s' % (func.__name__, err)
+        return result
+    return decorated
 
 def get_abs_ct_url(url):
     """ Return absolute url for CT page """
@@ -22,9 +25,9 @@ def get_abs_ct_url(url):
 def parse_ep_text(text):
     """ Return ep info from text """
     m = re.search('(?:S|Season )(?P<season>\d+)'
-                                         ' (?:E|Epis[o0]de )(?P<ep>\d+(?:-\d+)?)'
-                                         '(?: ?\W+)? (?P<name>.*)',
-                                         text)
+                             ' (?:E|Epis[o0]de )(?P<ep>\d+(?:-\d+)?)'
+                             '(?: ?\W+)? (?P<name>.*)',
+                             text)
     season = int(m.group('season'))
     ep = m.group('ep') # left as a string, to handle 15-16 situations
     name = m.group('name')
@@ -77,10 +80,12 @@ class ShowPage(CtPage):
     def get_ep_list(self, season=None):
         """ Return episode list for single or all seasons """
         eps = []
-        for li in self.entry.findall('li'):
+        for li in self.entry.find_all('li'):
             ep = parse_ep_text(li.text)
             if ep:
                 if not season or ep['season'] == season:
                     eps.append(ep)
         return eps
+
+
 
