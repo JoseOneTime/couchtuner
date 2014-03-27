07e793a6f8fd5f352c0fb55f460706bde281abf3
JoseOneTime <claveren@gmail.com>
2014-03-27 19:14:53 -0400

    Add common.py for common stuff

---
+++ py/common.py
@@ -0,0 +1,10 @@
+""" Common constants, classes and functions """
+
+HOSTS = {
+    'vidbull.com': 3000,
+    'vk.com': 1400,
+    'played.to': 1200,
+    'vshare.eu': 1100,
+    'youwatch.org': 1000
+}
+
